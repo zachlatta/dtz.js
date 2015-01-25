3a92fba67d42a2cefdc28664075dd49541cc53e2
Andy Fang <andy@fang.ws>
2015-01-25 15:29:23 -0800

    Same features as previous commit with minor bug fixes.

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,67 @@
 
 $(document).ready(function(){
-    $start = $('#start');
+    $start = $('#random');
     $macbook = $('.md-macbook-pro');
     $iphone = $('.md-iphone-5');
     $search = $('#search');
+    $device_screen = $('.md-screen');
     $macbook.hide();
-
     $iphone.hide();
+
+
+$start.on('click', function(){
+    $(this).fadeOut();
+});
+
+function get_entry(id) {
+    $.ajax({
+    url: "/conversations/" + id,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+        
+        data.forEach(function(item) {
+            sender = item['sender'];
+            console.log(item.contents);
+            if ((sender == 'me') && (item.contents.length > 0)) {
+                $device_screen.append('<p class="message bot"><br>' + item.contents + '</p>');
+            } else {
+                $device_screen.append('<p class="message"><br>' + item.contents + '</p>');
+            }
+
+        });
+
+
+
+        if (data.platform == "omegle"){
+            $iphone.fadeIn();
+        } else {
+            $macbook.fadeIn();
+        }
+    },
+    error : function(error) {
+        console.log("ERROR " + error);
+        console.log(error);
+    }
+});
+}
+
+$start.on('click', function(){
+    $.ajax({
+        url: "/conversations/",
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var index = Math.floor(Math.random()*data.length);
+            var entry = data[index];
+            console.log(entry);
+            get_entry(entry.id);
+        }
+    });
+});
+
+
+
 
     $search.on('input', function(){
         q = $(this).val();
@@ -17,35 +72,6 @@ $(document).ready(function(){
         }
         console.log(q);
 
-        $.ajax({
-            url: "/conversations/" + q,
-            type: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                $device_screen = $('.md-screen');
-                data.forEach(function(item) {
-                    sender = item['sender'];
-                    console.log(item.contents);
-                    if ((sender == 'me') && (item.contents.length > 0)) {
-                        $device_screen.append('<p class="message bot"><br>' + item.contents + '</p>');
-                    } else {
-                        $device_screen.append('<p class="message"><br>' + item.contents + '</p>');
-                    }
-
-                });
-
-
-
-                if (data.platform == "omegle"){
-                    $iphone.fadeIn();
-                } else {
-                    $macbook.fadeIn();
-                }
-            },
-            error : function(error) {
-                console.log("ERROR " + error);
-                console.log(error);
-            }
-        });
+        get_entry(q);
     });
 });
