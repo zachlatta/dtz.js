f0d2147985c12e737fd3123e725c2351ba7b2048
Zach Latta <zach@zachlatta.com>
2015-01-25 01:20:45 -0800

    zork

--- worker/run_zork.sh
+++ worker/run_zork.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 # Crazy magic sed-fu that gets rid of the ncurses bullshit.
-./zork "$@" | sed 's/Score:.*Moves:/\n/' | tail -n+3 | sed 's/.$//' | sed 's/.*>//' | sed "s/\r\x1b\[/\n/g" | ./strip_control_chars | sed "s/^[0-9d][0-9d][0-9d]//" | sed "s/\x1b(B\x1b\[m//" | sed "s/\x1b\[34;1H//" | tail -n+3
+output=$(./zork "$@" | sed 's/\r/\n/g' | ./strip_control_chars | sed 's/^[ \t]*//' | sed 's/[ \t]*$//' | tail -n+7 | sed '$d')
+
+if [ $# -eq 0 ]; then
+  echo "$output"
+else
+  tmp="$output"
+  for arg in $*; do
+    tmp=$(echo "$tmp" | sed '1,/>.*$/d')
+  done
+  echo "$tmp"
+fi
