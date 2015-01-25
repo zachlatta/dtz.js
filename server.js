b30a2439564269072460b403b58380527be655e1
Luke Wright <luke@simplyluke.com>
2015-01-25 11:50:11 -0800

    make express serve static out of public

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 var _ = require('underscore');
 var knex = require('knex')({
   client: 'pg',
   connection: process.env.PG_CONNECTION_STRING
 });
 
+
+app.get('/', function( req, res ) {
+  res.render('index');
+});
 app.get('/omegle/:chat_id', function(req, res) {
   var omegle = knex('messages')
     .where('conversation', req.params.chat_id)
@@ -25,4 +33,4 @@ app.get('/tinder', function(req, res) {
   res.send('yo, tinder');
 });
 
-app.listen(3001);
+app.listen(process.env.PORT || 3000);
