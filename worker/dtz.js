04644d66cb5a394c0ed7e62536526a38b0fb656b
Luke Wright <luke@simplyluke.com>
2015-01-25 10:41:07 -0800

    server is good

--- worker/dtz.js
+++ worker/dtz.js
@@ -16,52 +16,60 @@ bot.authorize(
 
     var defaults = bot.getDefaults()
     var recs_size = defaults.globals.recs_size;
-
-    setInterval(function () {
-      bot.getUpdates(function (err, updates) {
-        _.each(updates.matches, function (match) {
-          //{ _id: '54c5222f487f377160d1cfd3',
-              //match_id: '54bebeb5d6035bca31b705f354c48e4f498bfaab7f328cba',
-                //to: '54c48e4f498bfaab7f328cba',
-                  //from: '54bebeb5d6035bca31b705f3',
-                    //message: 'I swiped right so fast almost broke my phone ...',
-                      //sent_date: '2015-01-25T17:04:47.083Z',
-                        //created_date: '2015-01-25T17:04:47.083Z',
-                          //timestamp: 1422205487083 }
-          _.each(match.messages, function (msg) {
-            console.log(msg);
-            db.select('id').from('conversations').where({partner_id: msg.match_id})
-            .then(function (ids, otherParams) {
-              if (ids.length) {
-                console.log('create new');
-                return db('messages').insert({
-                  conversation: ids[0],
-                  sender: 'them',
-                  contents: msg.message
-                });
-              } else {
-                return db('conversations')
-                  .returning('id')
-                  .insert({
-                    platform: 'tinder',
-                    partner_id: msg.match_id
-                  })
-                  .then(function (convIds) {
-                    return db('messages').insert({
-                      conversation: convIds[0],
-                      sender: 'them',
-                      contents: msg.message
-                    });
-                  });
-              }
-            })
-            .catch(function (err) {
-              console.error(err);
-            });
+    bot.getRecommendations(recs_size, function(error, data){
+      _.chain(data.results)
+        .pluck('_id')
+        .each(function(id) {
+          bot.like(id, function(error, data) {
+            console.log("liked: " + id);
           });
         });
-      });
-    }, 1000); 
+    });
+    // setInterval(function () {
+    //   bot.getUpdates(function (err, updates) {
+    //     _.each(updates.matches, function (match) {
+    //       //{ _id: '54c5222f487f377160d1cfd3',
+    //           //match_id: '54bebeb5d6035bca31b705f354c48e4f498bfaab7f328cba',
+    //             //to: '54c48e4f498bfaab7f328cba',
+    //               //from: '54bebeb5d6035bca31b705f3',
+    //                 //message: 'I swiped right so fast almost broke my phone ...',
+    //                   //sent_date: '2015-01-25T17:04:47.083Z',
+    //                     //created_date: '2015-01-25T17:04:47.083Z',
+    //                       //timestamp: 1422205487083 }
+    //       _.each(match.messages, function (msg) {
+    //         console.log(msg);
+    //         db.select('id').from('conversations').where({partner_id: msg.match_id})
+    //         .then(function (ids, otherParams) {
+    //           if (ids.length) {
+    //             console.log('create new');
+    //             return db('messages').insert({
+    //               conversation: ids[0],
+    //               sender: 'them',
+    //               contents: msg.message
+    //             });
+    //           } else {
+    //             return db('conversations')
+    //               .returning('id')
+    //               .insert({
+    //                 platform: 'tinder',
+    //                 partner_id: msg.match_id
+    //               })
+    //               .then(function (convIds) {
+    //                 return db('messages').insert({
+    //                   conversation: convIds[0],
+    //                   sender: 'them',
+    //                   contents: msg.message
+    //                 });
+    //               });
+    //           }
+    //         })
+    //         .catch(function (err) {
+    //           console.error(err);
+    //         });
+    //       });
+    //     });
+    //   });
+    // }, 1000); 
 
     // bot.getHistory(function(error, data) {
     //   _.chain(data.matches)
--- worker/server.js
+++ worker/server.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
+var knex = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING
+});
+
+app.get('/omegle/:chat_id', function(req, res) {
+  var omegle = knex('messages')
+    .where('conversation', req.params.chat_id)
+    .select('id', 'sender', 'contents')
+    .then(function (rows) {
+    res.send(rows);
+  });
+});
 
 app.get('/omegle', function(req, res) {
-  res.send('yo, omegle');
+  knex('conversations').select('*').then(function (conversations) {
+    res.send(conversations);
+  });
 });
 
 app.get('/tinder', function(req, res) {
   res.send('yo, tinder');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
