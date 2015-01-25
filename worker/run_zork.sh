e5be576d38dd518a088dce8d3aa64f8a6afa98c7
Zach Latta <zach@zachlatta.com>
2015-01-24 23:28:16 -0800

    make it gooder

--- worker/run_zork.sh
+++ worker/run_zork.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Crazy magic sed-fu that gets rid of the ncurses bullshit.
-./zork "$@" | sed 's/.$//' | sed 's/.*>//' | sed "s/\r\x1b\[/\n/g" | ./strip_control_chars | sed "s/^[0-9d][0-9d][0-9d]//" | sed "s/\x1b(B\x1b\[m//" | sed "s/\x1b\[34;1H//" | tail -n+3
+./zork "$@" | sed 's/Score:.*Moves:/\n/' | tail -n+3 | sed 's/.$//' | sed 's/.*>//' | sed "s/\r\x1b\[/\n/g" | ./strip_control_chars | sed "s/^[0-9d][0-9d][0-9d]//" | sed "s/\x1b(B\x1b\[m//" | sed "s/\x1b\[34;1H//" | tail -n+3
