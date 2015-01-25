a8cb58f1fddcccab7d15a1f9ad5fb1f14c1afd82
Andy Fang <andy@fang.ws>
2015-01-25 14:24:02 -0800

    msg

--- public/js/app.js
+++ public/js/app.js
@@ -3,73 +3,40 @@ $(document).ready(function(){
     $start = $('#start');
     $macbook = $('.md-macbook-pro');
     $iphone = $('.md-iphone-5');
-
+    $search = $('#search');
     $macbook.hide();
 
     $iphone.hide();
 
-    $start.on('click', function(){
-        
+    $search.on('input', function(){
+        q = $(this).val();
+        console.log(q);
 
         $.ajax({
-        url:"/conversations",
-        type: 'GET',
-        dataType: 'json',
-        success:function(data){
-            data.forEach(function(item) {
-
-                $('#ids').append('<a class="collection-item" data-id="' + item.id + '">' + item.id + '</a>');
-
+            url: "/conversations/" + q,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                $iphone_screen = $('.md-screen');
+                data.forEach(function(item) {
+                    sender = item['sender'];
+                    console.log(item.contents);
+                    if (sender == 'them') {
+                        $iphone_screen.append('<p class="message"><br>' + item.contents + '</p>');
+                    } else {
+                        $iphone_screen.append('<p class="message bot"><br>' + item.contents + '</p>');
+                    }
                 });
 
-            $('#ids a').on('click', function(){
-            id = $(this).data('id');
-            console.log("CLICKED " + id);
-
-            $.ajax({
-                url: "/conversations/" + id,
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
 
 
-
-                    $iphone_screen = $('.md-screen');
-                    console.log("SUCCESS " + data);
-                    $('#ids').fadeOut();
-
-
-                    data.forEach(function(item) {
-                        sender = item['sender'];
-                        console.log(item.contents);
-                        if (sender == 'them') {
-                            $iphone_screen.append('<p class="message"><br>' + item.contents + '</p>');
-                        } else {
-                            $iphone_screen.append('<p class="message bot"><br>' + item.contents + '</p>');
-                        }
-                    });
-
-
-
-                    $macbook.fadeIn();
-                    $iphone.fadeIn();
-                },
-                error : function(error) {
-                    console.log("ERROR " + error);
-                    console.log(error);
-                }
-            });
-            });
-
-            
-            /*
-            id = data.id;
-
-            console.log(id);
-            console.log(data.platform);
-            */
-
-
-          }});
+                $macbook.fadeIn();
+                $iphone.fadeIn();
+            },
+            error : function(error) {
+                console.log("ERROR " + error);
+                console.log(error);
+            }
+        });
     });
 });
